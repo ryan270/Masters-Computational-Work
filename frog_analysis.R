961849ca61b15d05782d57b6422b0d36e60b9773
ry <ryan.fergusson.270@gmail.com>
2020-12-07 19:01:47 -0800

    Subset Analysis Update:1
    Pruned samples w/ < 80% relative abundance of Proteobacteria. Both the
    weighted and unweighted unifrac shows clearer separation than all
    previous analyses

--- frog_analysis.R
+++ frog_analysis.R
@@ -3,8 +3,8 @@
 #...of the microbial diversity of just salamanders.
 #This script is a modification of meta_analyze: the Meta Analysis Script
 setwd('~/Documents/amphibian_meta_project/meta_analysis/qiime_analyses/')
-project_packages <- c('phyloseq', 'qiime2R','DESeq2', 'phangorn', 'grid', 'ggplot2','DECIPHER',
-                      'gridExtra', 'vegan', 'wesanderson', 'dplyr', 'ggmap')
+project_packages <- c('phyloseq', 'qiime2R','phangorn', 'grid', 'ggplot2',
+                      'gridExtra', 'vegan', 'wesanderson', 'dplyr')
 sapply(project_packages, require, character.only = TRUE)
 
 #Create Phyloseq Object / Load data / Filter Ambiguous Orders
@@ -12,18 +12,61 @@ amphib.obj <- subset_taxa(qza_to_phyloseq(features="meta_c2_phy_table.qza", taxo
                                           tree = "meta_rootd.qza", metadata="merged_metadata.txt"),
                           !is.na(Order) & !Order %in% c("", "uncharacterized"))
 
-#Subset just salamanders
+#Subset just frogs
 frgs <- subset_samples(amphib.obj, Order =="Anura")
-brkf <- prune_samples(sample_sums(frgs) < 40000, frgs)
 
 #Very Important
 the.royal <- c("#899DA4", "#9A8822", "#F5CDB4", "#F8AFA8", "#FDDDA0", "#EE6A50", "#74A089")
 
 #----------------------------------------------------------------#
 #----------------------------------------------------------------#
+##EXPLORATORY ANALYSIS: Explore ideas to parse frog data
+#Remove Samples w/ more than 75% Proteobacteria
+tfrg <- transform_sample_counts(frgs, function(x) x/ sum(x)) %>%
+    psmelt()
+prof <- tfrg$Sample[which(tfrg$Phylum == "Proteobacteria" &
+                          tfrg$Abundance > .8)]
+nsmps <- setdiff(sample_names(frgs), prof)
+nopro <- prune_samples(nsmps, frgs)
+
+#----------------------------------------------------------------#
+##TAXA BARPLOT: Displays only the top OTUS for Each Region
+#Create Database of OTU's w/ 1% Category
+txs <- psmelt(tax_glom(transform_sample_counts(nopro, function(x) x / sum(x) ), taxrank = 'Phylum'))
+txs$Phylum <- as.character(txs$Phylum)
+txs$Phylum[txs$Abundance < 0.01] <- "< 1% Abundance"
+
+#Re-Order Levels
+txs$Phylum <- factor(txs$Phylum,
+                          levels = c("Acidobacteria", "Actinobacteria", "Armatimonadetes", "Bacteroidetes",
+                                     "Chlamydiae", "Chloroflexi", "Cyanobacteria", "Deferribacteres",
+                                     "Elusimicrobia", "Fibrobacteres", "Firmicutes", "Fusobacteria",
+                                     "Gemmatimonadetes", "Lentisphaerae", "Nitrospirae","Planctomycetes",
+                                     "Proteobacteria","TM7", "Verrucomicrobia", "WS3", "[Thermi]",
+                                     "< 1% Abundance"))
+
+#Plot Relative Abundances
+ggplot(txs, aes(x=Sample, y=Abundance, fill=Phylum))+
+  facet_wrap(~State_Region, scales = "free_x", nrow = 3)+
+  geom_bar(aes(), stat="identity", position="stack") +
+  scale_fill_manual(values = c("#E1BD6D", "#74A089", "#EABE94", "#FDDDA0", "#78B7C5", "#FF0000", "#00A08A",
+                               "#F2AD00", "#F98400", "#46ACC8", "#ECCBAE", "#F5CDB4", "#D69C4E", "#ABDDDE",
+                               "#446455", "#FDD262", "#EE6A50", "#899DA4","#D3DDDC", "#9A8822", "#046C9A",
+                               "#000000"))+
+  ylab('Relative Abunance')+
+  theme(legend.position= c(0.67, 0.17), legend.key.height = unit(0.7, 'cm'), legend.key.width = unit(1.7, 'cm'),
+        axis.title = element_text(size = 16, family = "Georgia"), axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(), axis.title.x = element_blank(),
+        legend.title = element_text(size = 14, family = "Georgia"),
+        legend.text = element_text(size = 11, family = "Georgia"),
+        panel.background = element_rect(fill = "gray98"), panel.grid.major = element_blank())+
+  guides(fill=guide_legend(nrow=6, title.position = 'top'),
+    theme(element_text(family = "Georgia")))
+
+#----------------------------------------------------------------#
 ##ALPHA DIVERSITY: plot and compare species richness and evenness
 #Calculate Evenness & Create DF with Evenness
-alphas <- estimate_richness(brkf, measure = c("Chao1", "Shannon", "Simpson"))
+alphas <- estimate_richness(nopro, measure = c("Chao1", "Shannon", "Simpson"))
 alphas$Evenness <- 0
 for(i in 1:nrow(alphas)){
   H <- alphas$Shannon
@@ -33,7 +76,7 @@ for(i in 1:nrow(alphas)){
 }
 
 #ANOVA Assumption Tests
-asa = merge(alphas, sample_data(brkf), by = 0, all = TRUE)
+asa = merge(alphas, sample_data(nopro), by = 0, all = TRUE)
 shapiro.test(alphas$Shannon) #FAILED: p = 0.02106
 bartlett.test(Evenness ~ State_Region, data = asa) #FAILED: p = 0.0049
 
@@ -43,7 +86,7 @@ bartlett.test(Evenness ~ State_Region, data = asa) #FAILED: p = 0.0049
 adonis(Evenness ~ State_Region, data = asa, permutations = 999, pairwise = TRUE) #Not even: p = 0.001
 
 #Plot Facet-Wrapped Boxpolot of Richness and Evenness
-alpha2 <- tidyr::gather(data.frame(alphas, sample_data(brkf)),
+alpha2 <- tidyr::gather(data.frame(alphas, sample_data(nopro)),
                         key = "Measure", value = "Value", Shannon, Chao1, Simpson, Evenness)
 ggplot(data = alpha2, aes(x = State_Region, y = Value, color = Family))+
   labs(color = "Host", x = "State Region")+
@@ -72,11 +115,11 @@ names(pca.list) = dist_models
 
 #For loop that loops through all of the distance models and calculates them
 for (i in dist_models) {
-  iDist <- phyloseq::distance(brkf, method=i)
-  iMDS  <- ordinate(brkf, "MDS", distance = iDist)
+  iDist <- phyloseq::distance(nopro, method=i)
+  iMDS  <- ordinate(nopro, "MDS", distance = iDist)
   #Make plot
   p <- NULL
-  p <- plot_ordination(brkf, iMDS, color="State_Region", shape="Order")+
+  p <- plot_ordination(nopro, iMDS, color="State_Region", shape="Order")+
     ggtitle(paste("Distance Method ", i, sep=""))+
     geom_point(size = 4)+
     theme(plot.title = element_text(size = 12, family = "Georgia"))
@@ -107,8 +150,8 @@ ggplot(adm, aes(Axis.1, Axis.2, color = State_Region, shape = Order))+
 
 #Use to Calculate Distances without For Loop on the Fly
 #Unweighted Unifrac -- The Plot
-ord <- ordinate(brkf, "MDS", distance = (phyloseq::distance(brkf, method = "wunifrac"))) #change model here
-plot_ordination(brkf, ord, color = "State_Region", shape = "Order")+
+ord <- ordinate(nopro, "MDS", distance = (phyloseq::distance(nopro, method = "unifrac"))) #change model here
+plot_ordination(nopro, ord, color = "State_Region", shape = "Order")+
   scale_color_manual(values = the.royal)+
   scale_fill_manual(values = the.royal)+
   geom_point(size = 5)+
@@ -130,8 +173,8 @@ plot_ordination(brkf, ord, color = "State_Region", shape = "Order")+
 #----------------------------------------------------------------#
 ##PERMANOVA: Confirms there are Diversity differences between the Groups
 #Significant differences in weighted and unweighted
-md = data.frame(sample_data(brkf))
-perm <- adonis(phyloseq::distance(brkf, method="unifrac") ~ State_Region,
+md = data.frame(sample_data(nopro))
+perm <- adonis(phyloseq::distance(nopro, method="wunifrac") ~ State_Region,
        data = md, permutations = 999)
 print(perm)
 
