888fb94ca0186ada1c570f432fccb66a782a47fd
ry <ryan.fergusson.270@gmail.com>
2021-03-01 09:22:35 -0800

    Updated seq_length label names

--- seq_lengths.R
+++ seq_lengths.R
@@ -6,7 +6,7 @@
 ##LOAD PACKAGES, DATA, AND DIRECTORY
 #Set Directory and Load required Packages
 setwd('~/Documents/amphibian_meta_project/meta_analysis/qiime_analyses/seq-metrics/')
-pcks <- c('ggplot2', 'wesanderson', 'rpsychi')
+pcks <- c('ggplot2', 'wesanderson')
 sapply(pcks, require, character.only = T)
 
 metrics <- read.csv(file = 'meta_bioinf_metrics.csv', header = TRUE, sep = ',',
@@ -20,15 +20,15 @@ ggplot(metrics, aes(x = Dataset, y = Mean_Seq_Lngth, group = 1))+
                 size = 1)+
   xlab('Dataset')+
   ylab('Mean Sequence Length')+
+  scale_x_discrete(breaks = c("Bird_et_al._2018", "Ellison_et_al._2018",
+                              "Ellison_et_al._2019", "Prado-Irwin_et_al._2017"),
+                   labels = c("Cal Salamanders Data", "Mexico-Guatemala Data",
+                              "Sierra Frogs Data", "Ensatina Data"))+
   theme_bw()+
   theme(legend.position = 'none',
         axis.title = element_text(size = 16, family = "Georgia"),
-        axis.text.x = element_text(size = 10, family = "Georgia"),
+        axis.text.x = element_text(size = 12, family = "Georgia"),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.border = element_blank(),
         axis.line = element_line(colour = 'black', size = 0.25))
-
-
-##ANOVA ANALYSIS
-with(metrics, ind.oneway.second(Mean_Seq_Lngth, Seq_SD, Samples))
