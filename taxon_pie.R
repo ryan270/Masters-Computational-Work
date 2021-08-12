9c96c3c7679828af2ff2b903b2666d5e1698c24c
ry <ryan.fergusson.270@gmail.com>
2021-08-12 15:24:06 -0700

    Implemented LSP changes to taxon_pie

--- taxon_pie.R
+++ taxon_pie.R
@@ -5,122 +5,122 @@
 # sample list ~/Documents/ensatina_x/qiime_work_2/sample_list.txt
 
 ##SET DIRECTORY AND LOAD PACKAGES
-setwd('/Users/ade/Documents/amphibian_meta_project/california_x/')
-pcks <- c('ggplot2', 'ggthemes', 'stringr')
+setwd("/Users/ade/Documents/amphibian_meta_project/california_x/")
+pcks <- c("ggplot2", "ggthemes", "stringr")
 sapply(pcks, require, character.only = T)
 require(data.table)
 
 #Load .tsv files
-tax.1 <- as.data.frame(fread('qiime_analyses/AB_taxonomy.tsv'))
-summary(tax.1)
+tax_1 <- as.data.frame(fread("qiime_analyses/AB_taxonomy.tsv"))
+summary(tax_1)
 
 ##FORMATTING
 #remove first row
-tax.1 <- tax.1[-c(1),]
+tax_1 <- tax_1[-c(1), ]
 
 #Rename default name to family name for the first pie chart
-for (i in tax.1$Taxon) {
-  if (str_detect(i, 'Proteobacteria') ==  TRUE) {
-    gsub('Proteobacteria', tax.1$Taxon)
+for (i in tax_1$taxon) {
+  if (str_detect(i, "Proteobacteria") ==  TRUE) {
+    gsub("Proteobacteria", tax_1$taxon)
   }
 }
 
 
-tax.1$Taxon <- (gsub('k__Bacteria; p__Chlamydiae; c__Chlamydiia;
-                     o__Chlamydiales; f__Chlamydiaceae',
-                     'Chlamydiaceae', tax.1$Taxon))
-tax.1$Taxon <- (gsub("k__Bacteria; p__Acidobacteria; c__Acidobacteriia;
+tax_1$taxon <- (gsub("k__Bacteria; p__Chlamydiae; c__Chlamydiia;
+                     o__Chlamydiales; f__Chlamydiaceae",
+                     "Chlamydiaceae", tax_1$taxon))
+tax_1$taxon <- (gsub("k__Bacteria; p__Acidobacteria; c__Acidobacteriia;
                      o__Acidobacteriales; f__Acidobacteriaceae;
                      g__Granulicella; s__paludicola",
-                     'Acidobacteriaceae', tax.1$Taxon))
-tax.1$Taxon <- (gsub("k__Bacteria; p__Bacteroidetes; c__Sphingobacteriia;
+                     "Acidobacteriaceae", tax_1$taxon))
+tax_1$taxon <- (gsub("k__Bacteria; p__Bacteroidetes; c__Sphingobacteriia;
                      o__Sphingobacteriales; f__Sphingobacteriaceae;
                      g__Mucilaginibacter; s__rigui",
-                     'Sphingobacteriaceae', tax.1$Taxon))
-tax.1$Taxon <- gsub("k__Bacteria; p__Proteobacteria; c__Gammaproteobacteria;
+                     "Sphingobacteriaceae", tax_1$taxon))
+tax_1$taxon <- gsub("k__Bacteria; p__Proteobacteria; c__Gammaproteobacteria;
                     o__Xanthomonadales; f__Xanthomonadaceae; g__Arenimonas;
                     s__oryziterrae",
-                    'Xanthomonadaceae', tax.1$Taxon)
-tax.1$Taxon <- gsub("k__Bacteria; p__Actinobacteria; c__Actinobacteria;
+                    "Xanthomonadaceae", tax_1$taxon)
+tax_1$taxon <- gsub("k__Bacteria; p__Actinobacteria; c__Actinobacteria;
                     o__Actinomycetales; f__Nocardioidaceae; g__Nocardioides",
-                    'Nocardioidaceae', tax.1$Taxon)
-tax.1$Taxon <- gsub("k__Bacteria; p__Proteobacteria; c__Gammaproteobacteria;
+                    "Nocardioidaceae", tax_1$taxon)
+tax_1$taxon <- gsub("k__Bacteria; p__Proteobacteria; c__Gammaproteobacteria;
                     o__Enterobacteriales; f__Enterobacteriaceae; g__Pseudomonas;
                     s__syringae",
-                    'Enterobacteriaceae', tax.1$Taxon)
-tax.1$Taxon <- gsub("k__Bacteria; p__Bacteroidetes; c__Sphingobacteriia;
+                    "Enterobacteriaceae", tax_1$taxon)
+tax_1$taxon <- gsub("k__Bacteria; p__Bacteroidetes; c__Sphingobacteriia;
                     o__Sphingobacteriales; f__Sphingobacteriaceae;
                     g__Cytophagales; s__MBIC4147",
-                    'Sphingobacteriaceae', tax.1$Taxon)
-tax.1$Taxon <- gsub("k__Bacteria; p__Actinobacteria; c__Actinobacteria;
+                    "Sphingobacteriaceae", tax_1$taxon)
+tax_1$taxon <- gsub("k__Bacteria; p__Actinobacteria; c__Actinobacteria;
                     o__Actinomycetales; f__Micromonosporaceae",
-                    'Micromonosporaceae', tax.1$Taxon)
+                    "Micromonosporaceae", tax_1$taxon)
 
 #blank theme for pie_chart
-blank_theme <- theme_minimal()+
+blank_theme <- theme_minimal() +
   theme(
     axis.title.x = element_blank(),
     axis.title.y = element_blank(),
     panel.border = element_blank(),
-    panel.grid=element_blank(),
+    panel.grid = element_blank(),
     axis.text = element_blank(),
     axis.ticks = element_blank(),
-    plot.title=element_text(size=14, face="bold")
+    plot.title = element_text(size = 14, face = "bold")
   )
 
 #TAXONOMY 1 PIE CHART!!
-#can't store plot as variable
-ggplot(tax.1, aes(x = factor(1), fill = Taxon))+
-  geom_bar(width = 1)+
-  coord_polar('y', start = 0)+
-  blank_theme+
-  labs(title = 'California Diversity',
-       subtitle = 'Preliminary Results')+
-  theme(plot.title = element_text(size = 32, face = "bold"))+
-  theme(plot.subtitle = element_text(size = 22, face = "italic"))+
-  theme(legend.position = 'none') #use in case you don't correct names
+#can"t store plot as variable
+ggplot(tax_1, aes(x = factor(1), fill = taxon)) +
+  geom_bar(width = 1) +
+  coord_polar("y", start = 0) +
+  blank_theme +
+  labs(title = "California Diversity",
+       subtitle = "Preliminary Results") +
+  theme(plot.title = element_text(size = 32, face = "bold")) +
+  theme(plot.subtitle = element_text(size = 22, face = "italic")) +
+  theme(legend.position = "none") #use in case you don"t correct names
 
-ggplot(data = tax.1, aes(x = Taxon))+
-  geom_col(stat = 'count')
+ggplot(data = tax_1, aes(x = taxon)) +
+  geom_col(stat = "count")
 
 
-#repeat for tax.2
-tax.2 <- tax.2[-c(1),]
+#repeat for tax_2
+tax_2 <- tax_2[-c(1), ]
 
-tax.2$Taxon <- gsub("k__Bacteria; p__Chlamydiae; c__Chlamydiia;
+tax_2$taxon <- gsub("k__Bacteria; p__Chlamydiae; c__Chlamydiia;
                     o__Chlamydiales; f__Parachlamydiaceae; g__; s__",
-                    'Parachlamydiaceae', tax.2$Taxon)
-tax.2$Taxon <- gsub("k__Bacteria; p__Bacteroidetes; c__Cytophagia;
+                    "Parachlamydiaceae", tax_2$taxon)
+tax_2$taxon <- gsub("k__Bacteria; p__Bacteroidetes; c__Cytophagia;
                     o__Cytophagales; f__Flammeovirgaceae; g__Flexithrix; s__",
-                    'Flammeovirgaceae', tax.2$Taxon)
-tax.2$Taxon <- gsub("k__Bacteria; p__Proteobacteria; c__Gammaproteobacteria;
+                    "Flammeovirgaceae", tax_2$taxon)
+tax_2$taxon <- gsub("k__Bacteria; p__Proteobacteria; c__Gammaproteobacteria;
                     o__Xanthomonadales; f__Xanthomonadaceae; g__; s__",
-                    'Xanthomonodaceae', tax.2$Taxon)
-tax.2$Taxon <- gsub("k__Bacteria; p__Actinobacteria; c__Actinobacteria;
+                    "Xanthomonodaceae", tax_2$taxon)
+tax_2$taxon <- gsub("k__Bacteria; p__Actinobacteria; c__Actinobacteria;
                     o__Actinomycetales; f__Streptomycetaceae; g__Streptomyces;
-                    s__", 'Streptomycetaceae', tax.2$Taxon)
-tax.2$Taxon <- gsub("k__Bacteria; p__Actinobacteria; c__Actinobacteria;
-                    o__Actinomycetales", 'Actinomycetales order', tax.2$Taxon)
-tax.2$Taxon <- gsub("k__Bacteria; p__Proteobacteria; c__Gammaproteobacteria;
+                    s__", "Streptomycetaceae", tax_2$taxon)
+tax_2$taxon <- gsub("k__Bacteria; p__Actinobacteria; c__Actinobacteria;
+                    o__Actinomycetales", "Actinomycetales order", tax_2$taxon)
+tax_2$taxon <- gsub("k__Bacteria; p__Proteobacteria; c__Gammaproteobacteria;
                     o__Pseudomonadales; f__Pseudomonadaceae; g__Pseudomonas;
-                    s__veronii", 'Pseudomonadaceae', tax.2$Taxon)
-tax.2$Taxon <- gsub("k__Bacteria; p__Proteobacteria; c__Gammaproteobacteria;
+                    s__veronii", "Pseudomonadaceae", tax_2$taxon)
+tax_2$taxon <- gsub("k__Bacteria; p__Proteobacteria; c__Gammaproteobacteria;
                     o__Pseudomonadales; f__Pseudomonadaceae; g__Pseudomonas;
-                    s__", 'Pseudomonadaceae', tax.2$Taxon)
-tax.2$Taxon <- gsub("k__Bacteria; p__Proteobacteria; c__Gammaproteobacteria;
+                    s__", "Pseudomonadaceae", tax_2$taxon)
+tax_2$taxon <- gsub("k__Bacteria; p__Proteobacteria; c__Gammaproteobacteria;
                     o__Pseudomonadales; f__Pseudomonadaceae; g__Pseudomonas",
-                    'Pseudomonadaceae', tax.2$Taxon)
-tax.2$Taxon <- gsub("k__Bacteria; p__Bacteroidetes", 'Bacteroidetes phylum',
-                    tax.2$Taxon)
+                    "Pseudomonadaceae", tax_2$taxon)
+tax_2$taxon <- gsub("k__Bacteria; p__Bacteroidetes", "Bacteroidetes phylum",
+                    tax_2$taxon)
 
 ##PLOT
 #TAXONOMY 2 PIE CHART
-ggplot(tax.2, aes(x = factor(1), fill = Taxon))+
-  geom_bar(width = 1)+
-  coord_polar('y', start = 0)+
-  blank_theme+
-  labs(title = 'Ensatina Microbial Diversity',
-       subtitle = 'First 100 reads, first pass at QIIME pipeline,
-       classifier re-trained')+
-  theme(plot.title = element_text(size = 20, face = "bold"))+
+ggplot(tax_2, aes(x = factor(1), fill = taxon)) +
+  geom_bar(width = 1) +
+  coord_polar("y", start = 0) +
+  blank_theme +
+  labs(title = "Ensatina Microbial Diversity",
+       subtitle = "First 100 reads, first pass at QIIME pipeline,
+       classifier re-trained") +
+  theme(plot.title = element_text(size = 20, face = "bold")) +
   theme(plot.subtitle = element_text(size = 14, face = "italic"))
