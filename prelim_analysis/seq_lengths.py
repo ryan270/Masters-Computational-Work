56fb88d8d368c40c3da52055959d34a46b9c633c
ry <ryan.fergusson.270@gmail.com>
2021-12-13 22:24:20 -0800

    Converted seq_lengths to Python

--- prelim_analysis/seq_lengths.py
+++ prelim_analysis/seq_lengths.py
@@ -1,14 +1,21 @@
-###SEQUENCE QUALITY METRICS
-#This script confirms the similarity of sequence metrics between the four
-# datasets.
-
-##LOAD PACKAGES, DATA, AND DIRECTORY
-#Set Directory and Load required Packages
-import pandas as pd
+# SEQ METRICS: Confirms/denies the uniformity of seq lengths across datasets
+from pandas import read_csv as pdr
 import seaborn as sns
-import matplotlib.pyplot as plt
-
-metrics = pd.read_csv("~/Documents/amphibian_meta_project/meta_analysis/" +
+from math import sqrt
+from matplotlib import pyplot as plt
+metrics = pdr("~/Documents/amphibian_meta_project/meta_analysis/" +
         "qiime_analyses/seq-metrics/meta_bioinf_metrics.csv")
+metrics['Seq_SE'] = metrics['Seq_SD']/sqrt(len(metrics['Seq_SD']))
 
-sns.plot(data=metrics, x=Dataset, y=Mean_Seq_Lngth)
+# Plot
+g = sns.FacetGrid(metrics, size=5)
+g.map(plt.errorbar, 'Dataset', 'Mean_Seq_Lngth', 'Seq_SE',
+        marker='o')
+g.set_xticklabels(["Ellison et al., 2019",
+        "Prado-Irwin et al., 2017",
+        "Ellison et al., 2018",
+        "Bird et al., 2018"])
+g.ylabel('Mean Sequence Length')
+plt.rcParams["axes.labelsize"] = 15
+plt.title('Similarity of Sequence Lenths', fontsize = 20)
+plt.show()
