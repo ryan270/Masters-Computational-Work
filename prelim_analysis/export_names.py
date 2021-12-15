5cc8719d1353d04d0ba0e3c5b5793f4c9324fca3
ry <ryan.fergusson.270@gmail.com>
2021-12-15 14:05:22 -0800

    Finished export_names

--- prelim_analysis/export_names.py
+++ prelim_analysis/export_names.py
@@ -15,20 +15,24 @@ se_d = pd.DataFrame({"Paper": ["Ellison et al., 2018"],
 mg = ampm.loc[ampm["Dataset"] == "Ellison et al., 2018"]
 mg_spp = mg['Species'].unique()
 mg_sam = []
+
 for reg in mg_spp:
-    mg_sam.append(len(mg.loc[mg['Species'] == reg])
+    mg_sam.append(len(mg.loc[mg['Species'] == reg]))
+
 mg_d = pd.DataFrame({'Paper': 'Ellison et al., 2018',
     'Species': mg_spp,
     'Region': 'Central America',
-    'N': mg_sam}
+    'N': mg_sam})
 
 # SPI Dataset
 spi = ampm.loc[ampm['Dataset'] == 'Prado-Irwin et al., 2017']
 spi_sam = []
+
 for reg in spi['State_Region'].unique():
-    spi_sam.append(len(spi.loc[spi['State_Region'] == reg])
+    spi_sam.append(len(spi.loc[spi['State_Region'] == reg]))
+
 spi_d = pd.DataFrame({'Paper': 'Prado-Irwin et al., 2017',
-    'Species': "Ensatina eschscholtzii",
+    'Species': 'Ensatina eschscholtzii',
     'Region': spi['State_Region'].unique(),
     'N': spi_sam})
 
@@ -37,10 +41,12 @@ ab = ampm.loc[ampm['Dataset'] == 'Bird et al., 2018']
 ab_spp = ab['Species'].unique()
 ab_reg = ab['State_Region'].unique()
 ab_sam = []
+
 for reg in ab_reg:
     k = ab.loc[ab['State_Region'] == reg]
     for j in ab_spp:
         ab_sam.append(len(k.loc[ab['Species'] == j]))
+
 from itertools import repeat
 ab_d = pd.DataFrame({'Paper': 'Bird et al., 2018',
     'Species': np.concatenate(list(repeat(ab_spp, 4))),
@@ -48,4 +54,10 @@ ab_d = pd.DataFrame({'Paper': 'Bird et al., 2018',
     'N': ab_sam}).sort_values(by=['Region'], ascending=True) # }}}
 
 ## {{{ MERGE, FORMAT, WRITE TABLE
-info_table = pd.DataFrame()
+info_table = pd.merge(se_d, mg_d, how='outer')
+info_table = pd.merge(info_table, spi_d, how='outer')
+info_table = pd.merge(info_table, ab_d, how='outer')
+import re
+info_table['Species'] = [re.sub("_", " ", spp) for spp in info_table['Species']]
+info_table.to_csv('~/Documents/amphibian_meta_project/Data_Info.csv')
+# }}}
