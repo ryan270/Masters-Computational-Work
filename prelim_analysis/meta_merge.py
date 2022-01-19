04fd091c4ddc4bc9ecff01bec94c24aa1cace16f
ry <ryan.fergusson.270@gmail.com>
2022-01-19 07:23:33 -0800

    Large Commit
    Implemented changes based on review.
    Separated Sierra to Highland & Lowland
    Added updated GPS to all samples
    Removed Bd_status

--- prelim_analysis/meta_merge.py
+++ prelim_analysis/meta_merge.py
@@ -1,10 +1,6 @@
 ## {{{ META MERGE: Merge mapping files from each dataset
-# Task: Need to separate Sierra Nevada into highland & lowland
-# Task: use the triangle and dot for frogs and salamanders in the California and the central america maps, then remove the legend in the central america map and add text to the figure legend that describes the triangles and points as such
-# Task: Remove County Data
 import pandas as pd
 import numpy as np
-
 abmap = pd.read_table("~/Documents/amphibian_meta_project/meta_analysis/" +
         "qiime_analyses/AB_mapping_file.txt", sep='\t')
 mgmap = pd.read_table('~/Documents/amphibian_meta_project/meta_analysis/' +
@@ -14,27 +10,62 @@ spimap = pd.read_table('~/Documents/amphibian_meta_project/meta_analysis/' +
 semap = pd.read_table('~/Documents/amphibian_meta_project/meta_analysis/' +
         'qiime_analyses/SE_mapping_file.txt', sep='\t') # }}}
 
+## {{{ DIVIDE CALIFORNIA INTO REGIONS
+abmap['State_Region'] = np.nan
+abmap.loc[(abmap['Site']=='Alameda') | (abmap['Site']=='Monterey'),
+    'State_Region'] = 'Coastal California'
+abmap.loc[(abmap['Site']=='Jackson_State_Forest') |
+        (abmap['Site']=='Siskiyou') | (abmap['Site']=='Shasta') |
+        (abmap['Site']=='Humboldt') | (abmap['Site']=='Leggett'),
+        'State_Region'] = 'Northern California'
+abmap.loc[(abmap['Site']=='Sierra_National_Forest'),
+        'State_Region'] = 'Sierra Lowland'
+abmap['State_Region'] = abmap['State_Region'].replace(
+        np.nan, 'Southern California')
+
+spimap['State_Region'] = np.nan
+spimap.loc[(spimap['Site']=='Alameda') | (spimap['Site']=='Santa.Cruz') |
+        (spimap['pop']=='north.bay'), 'State_Region'] = 'Coastal California'
+spimap.loc[(spimap['Site']=='Madera'), 'State_Region'] = 'Sierra Lowland'
+spimap.loc[(spimap['pop']=='annadel'), 'State_Region'] = 'Northern California'
+spimap['State_Region'] = spimap['State_Region'].replace(np.nan,
+        'Southern California')
+spimap['State_Region'].value_counts()
+
+semap['State_Region'] = 'Sierra Highland'
+semap['Latitude'] = np.nan
+semap['Longitude'] = np.nan
+semap.loc[(semap['site_id']=='Pyramid_Valley'), 'Latitude'] = 38.825959
+semap.loc[(semap['site_id']=='Rivendell'), 'Latitude'] = 38.848118
+semap.loc[(semap['site_id']=='Pyramid_Valley'), 'Longitude'] = -120.142295
+semap.loc[(semap['site_id']=='Rivendell'), 'Longitude'] = -120.163238 # }}}
+
 ## {{{ FORMAT COLUMNS
-abmap = abmap.drop(['Sample_Type', 'Higher_Clade'], axis=1)
-mgmap = mgmap.drop(['LinkerPrimerSequence', 'ReversePrimer', 'BarcodeSequence',
-    'Sex', 'Age_Class', 'Geology', 'Site', 'Mountain_range', 'Collector_number',
-    'ZE', 'Log_ZE', 'Elevation', 'Description', 'Date', 'Country'], axis=1)
-spimap = spimap.drop(['LinkerPrimerSequence', 'ReversePrimer', 'BarcodeSequence',
-    'project1', 'project2', 'type', 'age', 'sex', 'cov.obj', 'soil.moist',
-    'soil.temp', 'cov.wd', 'cov.lth', 'date', 'Description', 'svl',
-    'tot.lgth', 'wgt'], axis=1)
-semap = semap.drop(['Sample_Name', 'month', 'frog_location', 'extraction_date',
-    'BarcodeSequence', 'year', 'frog_id', 'survey_date', 'frog_weight',
-    'Description', 'LinkerPrimerSequence', 'lifestage', 'ZE', 'pit_tag_id',
-    'frog_svl', 'ReversePrimer', 'gosner_stage', 'frog_sex',
-    'swabber_name', 'site_id'], axis=1)
-abmap = abmap.rename(columns={'Habitat_Type':'Habitat',
-    'Lower_Clade':'subspecies'})
-mgmap = mgmap.rename(columns={'County_Municipio': 'Site',
-    'Bd_Status':'Bd_status'})
+abmap = abmap.drop(['Higher_Clade', 'Lower_Clade', 'Habitat_Type',
+    'Bd_status'], axis=1)
+mgmap = mgmap.drop(['BarcodeSequence', 'LinkerPrimerSequence', 'ReversePrimer',
+    'Collector_number', 'subspecies', 'Country', 'County_Municipio', 'Site',
+    'Date', 'Sex', 'Age_Class', 'ZE', 'Log_ZE', 'Elevation', 'Mountain_range',
+    'Geology', 'Habitat', 'Description', 'Bd_Status'], axis=1)
+spimap = spimap.drop(['BarcodeSequence', 'LinkerPrimerSequence',
+    'ReversePrimer', 'type', 'project1', 'project2', 'subspecies', 'pop', 'age',
+    'sex', 'cov.obj', 'svl', 'tot.lgth', 'wgt', 'soil.moist', 'soil.temp',
+    'cov.wd', 'cov.lth', 'date', 'Description'], axis=1)
+semap = semap.drop(['BarcodeSequence', 'LinkerPrimerSequence', 'ReversePrimer',
+    'Sample_Name', 'frog_id', 'lifestage', 'gosner_stage', 'month', 'year',
+    'ZE', 'survey_date', 'pit_tag_id', 'frog_sex', 'extraction_date',
+    'frog_location', 'frog_weight', 'frog_svl', 'swabber_name', 'Description',
+    'Bd_status'],
+    axis=1)
+
+abmap = abmap.rename(columns={'Sample_Type':'Order'})
+semap = semap.rename(columns={'site_id':'Site'})
+mgmap = mgmap.rename(columns={'State_Province': 'Site'})
 spimap = spimap.rename(columns={'county': 'Site',
     'lat':'Latitude', 'long':'Longitude'})
-mgmap=mgmap.drop(mgmap.index[78:])
+mgmap = mgmap.drop([0, 78, 79, 80, 81, 82])
+mgmap['Latitude'] = mgmap['Latitude'].astype('float')
+mgmap['Longitude'] = mgmap['Longitude'].astype('float')
 mgmap['State_Region'] = "Central America"
 abmap['Dataset'] = "Bird et al., 2018"
 mgmap['Dataset'] = "Ellison et al., 2018"
@@ -53,39 +84,17 @@ semap['Genus'] = "Rana"
 semap['Family'] = "Ranidae"
 semap['Order'] = "Frog"
 mgmap['Order'] = mgmap['Order'].apply(lambda x: 'Salamander' if x=='Caudata'
-        else 'Frog')
-abmap['Bd_status'] = abmap['Bd_status'].apply(lambda x: 0 if x=='Negative'
-        else 1)
-semap['Bd_status'] = semap['Bd_status'].apply(lambda x: 0 if x=='Negative'
-        else 1) # }}}
-
-## {{{ DIVIDE CALIFORNIA INTO FOUR REGIONS
-abmap['State_Region'] = np.nan
-abmap.loc[(abmap['Site']=='Alameda') | (abmap['Site']=='Monterey'),
-    'State_Region'] = 'Coastal California'
-abmap.loc[(abmap['Site']=='Jackson_State_Forest') |
-        (abmap['Site']=='Siskiyou') | (abmap['Site']=='Shasta') |
-        (abmap['Site']=='Humboldt') | (abmap['Site']=='Leggett'),
-        'State_Region'] = 'Northern California'
-abmap.loc[(abmap['Site']=='Sierra_National_Forest'),
-        'State_Region'] = 'Sierra Nevada'
-abmap['State_Region'] = abmap['State_Region'].replace(
-        np.nan, 'Southern California')
-spimap['State_Region'] = np.nan
-spimap.loc[(spimap['Site']=='Alameda') | (spimap['Site']=='Santa.Cruz') |
-        (spimap['pop']=='north.bay'), 'State_Region'] = 'Coastal California'
-spimap.loc[(spimap['Site']=='Madera'), 'State_Region'] = 'Sierra Nevada'
-spimap.loc[(spimap['pop']=='annadel'), 'State_Region'] = 'Northern California'
-spimap['State_Region'] = spimap['State_Region'].replace(
-        np.nan, 'Southern California')
-spimap['State_Region'].value_counts()
-semap['State_Region'] = 'Sierra Nevada' # }}}
+        else 'Frog') # }}}
 
 ## {{{ JOIN & EXPORT TABLES ------
-pd.merge(semap, mgmap, on=["SampleID", "Bd_status", "State_Region", "Dataset",
-     "Longitude", "Latitude", "Family", "Genus", "Species", "Order"], how='full')
+dfs = [abmap, spimap, mgmap, semap]
 
-meta_3.to_csv('~/Documents/amphibian_meta_project/meta_analysis/' +
+from functools import reduce
+mm = reduce(lambda left,right: pd.merge(left,right, on=["SampleID",
+    "State_Region", "Dataset", "Longitude", "Latitude", "Family", "Genus",
+    "Species", "Order", "Site"], how='outer'), dfs)
+
+mm.to_csv('~/Documents/amphibian_meta_project/meta_analysis/' +
         'qiime_analyses/merged_metadata.txt', sep="\t", header=True,
-        Index=False)
+        index=False)
 # }}}
